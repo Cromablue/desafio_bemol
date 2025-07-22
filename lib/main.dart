8ab917a8e0a2850f98ca40c8050083b2ac095f63
CROMABLUE <jsmoura.dev@gmail.com>
2025-07-22 16:55:44 -0400

    :sparkles: Feat: project basic structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,29 @@
 import 'package:flutter/material.dart';
 
-main(){
-  runApp(Container(
-    child: Center(
-      child: Text(
-        'Bem vindo, ao seu app de consulta de cotações', textDirection: TextDirection.ltr,
-        style: TextStyle(color: Colors.white, fontSize: 60.0),
+void main() {
+  runApp(CurrencyApp());
+}
+
+class CurrencyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Cotações Financeiras',
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cotações Financeiras'),
       ),
-    )
-  ));
+      body: Center(
+        child: Text('Olá, Mundo!'),
+      ),
+    );
+  }
 }
