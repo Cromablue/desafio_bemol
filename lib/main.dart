2bc47a3e56026ab0d2d92417fa0e7b650c1b5a55
CROMABLUE <jsmoura.dev@gmail.com>
2025-07-26 13:52:44 -0400

    :rocket: chore: Initialize app and global services

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:timeago/timeago.dart' as timeago;
 import 'app/currency_app.dart';
 
-void main() {
-  runApp(CurrencyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
+  await initializeDateFormatting('pt_BR');
+  timeago.setLocaleMessages('pt_BR', timeago.PtBrMessages());
+  runApp(const CurrencyApp());
 }
